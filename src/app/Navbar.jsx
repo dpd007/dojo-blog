f6d0c774a76516f572ca84198fcf4e128290b52e
dpd007 <dasdurga923@gmail.com>
2021-08-26 21:23:18 +0530

    implementing the inline and external styles

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,13 +1,21 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>dojo blog</h1>
-            <div className="links">
-                <a href="/">home</a>
-                <a href="/create">new blog</a>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>dojo blog</h1>
+      <div className="links">
+        <a href="/">home</a>
+        {/* implementing the inline style */}
+        <a href="/create" style={{
+            color : "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px"
+        }}>
+          new blog
+        </a>
+        {/* implementing the inline style */}
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
