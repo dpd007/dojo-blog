34e5d9ca76212d1414a374a185d978d81d29a087
dpd007 <dasdurga923@gmail.com>
2021-08-26 21:28:29 +0530

    learning the handling event

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,9 +1,19 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h1>Home Page</h1>
-        </div>
-     );
-}
- 
+  const handleClick = (e) => {
+    console.log("Hello Dugu!!", e);
+  };
+  const handleClickAgain = (name) => {
+    console.log("Hello " + name);
+  };
+  return (
+    <div className="home">
+      <h1>Home Page</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => handleClickAgain("gayatree")}>
+        Click Me Again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
