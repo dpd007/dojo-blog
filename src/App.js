f2e52fce3d3a82c12d966a71302e3c0bd35a709a
dpd007 <dasdurga923@gmail.com>
2021-08-27 01:50:47 +0530

    Route parameters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./app/Navbar";
 import "./app/css/style.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./app/Create";
+import BlogDetails from "./app/BlogDetails";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/create" component={Create} />
+            {/* :id means a dynamic value */}
+            <Route exact path="/blogs/:id" component={BlogDetails} />
           </Switch>
         </div>
       </div>
--- src/app/BlogLists.jsx
+++ src/app/BlogLists.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 const BlogsList = ({ blogs, title, deleteBlog }) => {
   return (
     <div className="blogs-list">
       <h2 style={{ marginLeft: "15px" }}>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>
-            Author : <i>{blog.author}</i>
-          </p>
-          <button onClick={() => deleteBlog(blog.id)}>delete blog</button>
+          <Link to={`/blogs/${blog.id}`} style={{ textDecoration: "none" }}>
+            <h2>{blog.title}</h2>
+            <p>
+              Author : <i>{blog.author}</i>
+            </p>
+          </Link>
         </div>
       ))}
     </div>
