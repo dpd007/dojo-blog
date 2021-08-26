8661624c3102b97c56043519409a9d222c5d93ab
dpd007 <dasdurga923@gmail.com>
2021-08-26 21:50:45 +0530

    Outputting the lists by using map()

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -9,16 +9,23 @@ const Home = () => {
   //   };
 
   //learning the useState Hook
-  const [name, setName] = useState("Dugu");
-  const [age, setAge] = useState(24);
+  //   const [name, setName] = useState("Dugu");
+  //   const [age, setAge] = useState(24);
 
-  const handleClick = () => {
-    setName("Gitu");
-    setAge(23);
-  };
+  //   const handleClick = () => {
+  //     setName("Gitu");
+  //     setAge(23);
+  //   };
+
+  //Outputting the Lists
+  const [blogs, setblogs] = useState([
+      {id:1, title: 'My new website', body: 'lorem ipsum...', author: 'Dugu'},
+      {id:2, title: 'Welcome to React', body: 'lorem ipsum...', author: 'Gitu'},
+      {id:3, title: 'Learning React', body: 'lorem ipsum...', author: 'Dev'}
+  ]);
   return (
     <div className="home">
-      <h1>Home Page</h1>
+      {/* <h1>Home Page</h1> */}
 
       {/* learning the handling event */}
       {/* <button onClick={() => handleClickAgain("gayatree")}>
@@ -26,10 +33,19 @@ const Home = () => {
       </button> */}
 
       {/* learning the useState Hook */}
-      <p>
+      {/* <p>
         {name} is {age} years old!!
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      </p> */}
+      {/* <button onClick={handleClick}>Click Me</button> */}
+
+      {/* Outputting the Lists using map() */}
+      {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id}>
+              <h2>{blog.title}</h2>
+              <p>{blog.body}</p>
+              <p>Author : <i>{blog.author}</i></p>
+          </div>
+      ))}
     </div>
   );
 };
