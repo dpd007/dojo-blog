3975cd446015025c9a7bf2e1c3b17e42d57daa2b
dpd007 <dasdurga923@gmail.com>
2021-08-27 00:36:24 +0530

    Using JSON Server for fetching the data with useEffect

---
+++ Readme.md
@@ -0,0 +1,7 @@
+##Dojo-Blog
+This is an application where i learn some of the basics in React.
+#####To run this, follow the steps:
+1.npm install
+2.npm run start
+Open another terminal and type:
+npx json-server --watch src/app/data/Blogs.json --port 8080
--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -19,26 +19,27 @@ const Home = () => {
   //   };
 
   //Outputting the Lists
-  const [blogs, setblogs] = useState([
-    { id: 1, title: "My new website", body: "lorem ipsum...", author: "Dugu" },
-    {
-      id: 2,
-      title: "Welcome to React",
-      body: "lorem ipsum...",
-      author: "Gitu",
-    },
-    { id: 3, title: "Learning React", body: "lorem ipsum...", author: "Dev" },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+
+  //using JSON Server to fetch the data
+  //END POINTS
+  // /blogs - fetch all
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setblogs(newBlogs);
+    setBlogs(newBlogs);
   };
 
   //   learning the useEffect Hook
   useEffect(() => {
-      console.log('useEffect ran!!!');
-  }, [blogs])
+    fetch("http://localhost:8080/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
   return (
     <div className="home">
       {/* <h1>Home Page</h1> */}
@@ -54,11 +55,13 @@ const Home = () => {
       </p> */}
       {/* <button onClick={handleClick}>Click Me</button> */}
       {/* using props to send data to another component*/}
-      <BlogsList
-        blogs={blogs}
-        title={"All Blogs are here."}
-        deleteBlog={handleDelete}
-      />
+      {blogs && (
+        <BlogsList
+          blogs={blogs}
+          title={"All Blogs are here."}
+          deleteBlog={handleDelete}
+        />
+      )}
 
       {/* using the filter() to filter out the particular data */}
       {/* <BlogsList
